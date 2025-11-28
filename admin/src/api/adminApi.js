e2e1d530f56ad0ce7da093696d98173179816bc1
Amar <amarjha880@gmail.com>
2025-11-28 12:27:10 +0530

    updated base url import

--- admin/src/api/adminApi.js
+++ admin/src/api/adminApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = `${import.meta.env.VITE_SERVER_BASE_URL}`;
+
 
 // ✅ Create a reusable axios instance
 const api = axios.create({
